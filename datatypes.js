de975907b5402f248db472c60e5023e15f4d52a0
Divyansh Dixit <divyanshdixit96@gmail.com>
2021-09-29 22:58:53 +0530

    data types, let and const

--- datatypes.js
+++ datatypes.js
@@ -1,1 +1,46 @@
-// coming soon
+// js variabe can hold many datatypes
+
+// two types of values:
+
+// primitive values  (immutable, not changed) => they don't have theri own props and method
+
+// non-primitive values (mutable , can change) => they have props and methods
+
+// Complex datatype: function(){} => js function are object 
+
+//Primitve :
+
+// String 
+// Number
+// Boolean
+// undefined
+
+// Non-primitive:
+
+// Object
+// Array => they are special kind of object
+// Null
+
+
+// const y = {
+//     fname:'Divyansh',
+//     lname:'Dixit'
+// } // 1001
+
+// y = '10'
+
+// const x = 10;
+
+var c = 10;
+
+var c = null; // js doesn't recommend 
+var d ;
+
+console.log(c == d)
+
+console.log( typeof null , typeof undefined, typeof function(){})
+console.log(null == undefined, null === undefined)
+console.log(c)
+
+
+// Hoisting:
